bc4da9cee251e584543a42d376cfc9c14452c025
Anthony Cueva <acueva@pcienergysolutions.com>
2025-04-27 15:20:47 -0500

    feat: parse product information and populate vector db

---
+++ src/labs/populate.ts
@@ -0,0 +1,21 @@
+import { vectorIndex } from "@/lib/vector";
+import { readFileSync } from "node:fs";
+import { join } from "node:path";
+
+async function main() {
+	const products = JSON.parse(
+		readFileSync(
+			join(__dirname, "data", "products-for-embedding.json"),
+			"utf8",
+		),
+	);
+	await vectorIndex.upsert(
+		products.map((product) => ({
+			id: product.id,
+			data: product.context,
+			metadata: product.metadata,
+		})),
+	);
+}
+
+main();
