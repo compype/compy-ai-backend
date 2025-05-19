4087f642a9cc1a85e42b3b3c3fec34c5f1a58437
Anthony Cueva <hi@cueva.io>
2025-05-19 12:39:24 -0500

    refactor: encapsulate search logic in IIFE for improved structure and maintainability, ensuring consistent header setup and request options across queries

--- cook/query.ts
+++ cook/query.ts
@@ -1,23 +1,26 @@
-const myHeaders = new Headers();
-myHeaders.append("X-TYPESENSE-API-KEY", process.env.TYPESENSE_API_KEY || "");
+(() => {
+  const myHeaders = new Headers();
 
-const requestOptions: RequestInit = {
-  method: "GET",
-  headers: myHeaders,
-  redirect: "follow"
-};
+  myHeaders.append("X-TYPESENSE-API-KEY", process.env.TYPESENSE_API_KEY || "");
 
-const searchParams = new URLSearchParams({
-  q: "macbook",
-  query_by: "title,repmodel",
-  sort_by: "top:desc,percent_offer:desc",
-  per_page: "5"
-});
+  const requestOptions: RequestInit = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow"
+  };
 
-fetch(`http://typesense-app-autoscaling-lb-290518720.us-west-2.elb.amazonaws.com/collections/products2/documents/search?${searchParams.toString()}`, requestOptions)
-  .then((response) => response.text())
-  .then((result) => {
-    const fs = require('node:fs');
-    fs.writeFileSync('cook/out.json', result);
-  })
-  .catch((error) => console.error(error));
+  const searchParams = new URLSearchParams({
+    q: "macbook",
+    query_by: "title,repmodel",
+    sort_by: "top:desc,percent_offer:desc",
+    per_page: "5"
+  });
+
+  fetch(`http://typesense-app-autoscaling-lb-290518720.us-west-2.elb.amazonaws.com/collections/products2/documents/search?${searchParams.toString()}`, requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      const fs = require("node:fs");
+      fs.writeFileSync("cook/out.json", result);
+    })
+    .catch((error) => console.error(error));
+})();
