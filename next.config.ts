1dcf6bd3ce4d01b006cc699a265626606ac6affc
Anthony Cueva <hi@cueva.io>
2025-05-26 13:21:49 -0500

    refactor: update CORS configuration in next.config.ts to allow all origins for improved flexibility

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,32 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        // Apply these headers to all routes
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: 'https://compy.pe',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, PUT, DELETE, OPTIONS',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'X-Requested-With, Content-Type, Authorization',
-          },
-          {
-            key: 'Access-Control-Allow-Credentials',
-            value: 'true',
-          },
-        ],
-      },
-    ];
-  },
+	async headers() {
+		return [
+			{
+				// Apply these headers to all routes
+				source: "/:path*",
+				headers: [
+					{
+						key: "Access-Control-Allow-Origin",
+						value: "*",
+					},
+					{
+						key: "Access-Control-Allow-Methods",
+						value: "GET, POST, PUT, DELETE, OPTIONS",
+					},
+					{
+						key: "Access-Control-Allow-Headers",
+						value: "X-Requested-With, Content-Type, Authorization",
+					},
+					{
+						key: "Access-Control-Allow-Credentials",
+						value: "true",
+					},
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;
