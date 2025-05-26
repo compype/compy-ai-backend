82703c6f08812e7617ea8f4e7ef3476fb74ba503
Anthony Cueva <hi@cueva.io>
2025-05-26 13:17:45 -0500

    refactor: remove middleware.ts and consolidate CORS handling in next.config.ts for improved configuration and maintainability

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,32 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	async rewrites() {
-		return {
-			beforeFiles: [
-				{
-					source: "/api/:path*",
-					has: [
-						{
-							type: "header",
-							key: "origin",
-						},
-					],
-					destination: "/api/:path*",
-				},
-			],
-		};
-	},
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://compy.pe',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, Content-Type, Authorization',
+          },
+          {
+            key: 'Access-Control-Allow-Credentials',
+            value: 'true',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
